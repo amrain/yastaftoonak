50001351509f0bcc62fb8642ab5bfe633f1b8816
Abood <abd.3mrain@gmail.com>
2026-04-13 10:02:14 +0300

    Fix CORS wildcard crash1

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const dotenv = require('dotenv');
 
-dotenv.config({ path: path.resolve(process.cwd(), '.env') });
-
+//dotenv.config({ path: path.resolve(process.cwd(), '.env') });
+dotenv.config();
 const env = {
   nodeEnv: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 5000,
